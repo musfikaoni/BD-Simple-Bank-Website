cc02cacc6a825e9ff8bf52368d4584fe942266e5
musfikaoni <musfikaaoni@gmail.com>
2024-03-19 11:31:23 +0600

    withdraw & deposit section

---
+++ dash.js
@@ -0,0 +1,7 @@
+document.getElementById('btn-deposit').addEventListener('click',function() {
+    const depositField = document.getElementById('deposit-field');
+    const newDepositString = depositField.ariaValueMax;
+    const newDeposit = parseFloat(newDepositString);
+
+    const depositTotalElement = document.getElementById
+})
