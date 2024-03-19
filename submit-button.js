f6b42e8bb90d4f5e1a894ead70d120847699ea82
musfikaoni <musfikaaoni@gmail.com>
2024-03-19 10:57:12 +0600

    amount section

--- submit-button.js
+++ submit-button.js
@@ -1,3 +1,14 @@
 document.getElementById('btn-submit').addEventListener('click', function(){
-    const emailField = document.getElementById
+    const emailField = document.getElementById('input-field');
+    const email = emailField.ariaValueMax;
+
+    const  passField = document.getElementById('pass-field');
+    const pass =passField.ariaValueMax;
+
+    if(email === 'musfika@gmail.com' && pass === 'musu123') {
+        window.location.href = 'dashboard.html';
+    }
+    else {
+        alert('"wrong" emaill or password');
+    }
 })
