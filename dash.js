805fa8097e13ab029d3a0568a3ef5cfcb62c6a40
musfikaoni <musfikaaoni@gmail.com>
2024-03-19 11:43:23 +0600

    dashboard deposit

--- dash.js
+++ dash.js
@@ -3,5 +3,19 @@ document.getElementById('btn-deposit').addEventListener('click',function() {
     const newDepositString = depositField.ariaValueMax;
     const newDeposit = parseFloat(newDepositString);
 
-    const depositTotalElement = document.getElementById
+    const depositTotalElement = document.getElementById('deposit-area');
+    const prevDepositString = depositTotalElement.innerText;
+    const prevDeposit = parseFloat(prevDepositString);
+
+    const currentDepositTotal = prevDeposit + newDeposit;
+    depositTotalElement.innerText = currentDepositTotal;
+
+    depositField.value = '';
+
+    const balanceTotalElement =document.getElementById('balance-field');
+    const prevBalanceTotalString = balanceTotalElement.innerText;
+    const pervBalanceTotal = parseFloat(prevBalanceTotalString);
+
+    const currentBalanceTotal = pervBalanceTotal + newDeposit;
+    balanceTotalElement.innerText = currentBalanceTotal;
 })
